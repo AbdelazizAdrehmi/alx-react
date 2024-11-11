098c502ef3b788482408a339903138342ebe4b0c
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-11-11 15:31:31 +0100

    CourseList.js

--- 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
@@ -1,33 +1,27 @@
-import React from "react";
-import "./CourseList.css";
-import CourseListRow from "./CourseListRow";
 import PropTypes from "prop-types";
-import CourseShape from "./CourseShape";
+import React, { Component } from "react";
+import BodySection from "./BodySection";
+import { StyleSheet, css } from "aphrodite";
 
-function CourseList({ listCourses }) {
-  return (
-    <table id="CourseList">
-      <thead>
-        <CourseListRow textFirstCell="Available courses" isHeader={true} />
-        <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
-      </thead>
-      <tbody>
-        {listCourses.length > 0 ? (
-          listCourses.map(({ id, name, credit }) => <CourseListRow key={id} textFirstCell={name} textSecondCell={credit} />)
-        ) : (
-          <CourseListRow textFirstCell="No course available yet" />
-        )}
-      </tbody>
-    </table>
-  );
+class BodySectionWithMarginBottom extends Component {
+  render() {
+    return (
+      <div className={css(styles.bodySectionWithMargin)}>
+        <BodySection {...this.props} />
+      </div>
+    );
+  }
 }
 
-CourseList.propTypes = {
-  listCourses: PropTypes.arrayOf(CourseShape),
+const styles = StyleSheet.create({
+  bodySectionWithMargin: {
+    marginBottom: "40",
+  },
+});
+
+BodySectionWithMarginBottom.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
 };
 
-CourseList.defaultProps = {
-  listCourses: [],
-};
-
-export default CourseList;
+export default BodySectionWithMarginBottom;
