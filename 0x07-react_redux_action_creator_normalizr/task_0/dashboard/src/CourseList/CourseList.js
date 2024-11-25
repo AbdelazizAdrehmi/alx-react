63a41fb638d09595a4fd015545a3072f28ea5891
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-11-25 15:29:56 +0100

    CourseList.js

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/CourseList/CourseList.js
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/CourseList/CourseList.js
@@ -1,27 +1,60 @@
-import PropTypes from "prop-types";
-import React, { Component } from "react";
-import BodySection from "./BodySection";
+import React from "react";
 import { StyleSheet, css } from "aphrodite";
+import CourseListRow from "./CourseListRow";
+import PropTypes from "prop-types";
+import CourseShape from "./CourseShape";
 
-class BodySectionWithMarginBottom extends Component {
-  render() {
-    return (
-      <div className={css(styles.bodySectionWithMargin)}>
-        <BodySection {...this.props} />
-      </div>
-    );
-  }
+function CourseList({ listCourses }) {
+  return (
+    <table id="CourseList" className={css(styles.table)}>
+      <thead>
+        <CourseListRow textFirstCell="Available courses" isHeader={true} />
+        <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+      </thead>
+      <tbody>
+        {listCourses.length > 0 ? (
+          listCourses.map(({ id, name, credit }) => <CourseListRow key={id} textFirstCell={name} textSecondCell={credit} />)
+        ) : (
+          <CourseListRow textFirstCell="No course available yet" />
+        )}
+      </tbody>
+    </table>
+  );
 }
 
 const styles = StyleSheet.create({
-  bodySectionWithMargin: {
-    marginBottom: "40",
+  table: {
+    marginTop: "2em",
+    width: "100%",
+    border: "1px solid #ddd",
+    fontSize: "1.2rem",
+    marginBottom: "15em",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+
+  th: {
+    borderBottom: "1px solid #ddd",
+    width: "80%",
+  },
+
+  td: {
+    width: "80%",
+  },
+
+  tr: {
+    "nth-child(2)": {
+      textAlign: "left",
+    },
   },
 });
 
-BodySectionWithMarginBottom.propTypes = {
-  title: PropTypes.string.isRequired,
-  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+CourseList.propTypes = {
+  listCourses: PropTypes.arrayOf(CourseShape),
 };
 
-export default BodySectionWithMarginBottom;
+CourseList.defaultProps = {
+  listCourses: [],
+};
+
+export default CourseList;
